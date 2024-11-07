17c62e8465ee587a3641293d343c082335e9935a
Supeng Yu <supeng.yu@uconn.edu>
2024-11-06 20:56:26 -0500

    Update main.py to let schedule.py work

--- backend/main.py
+++ backend/main.py
@@ -11,6 +11,7 @@ from user import user_bp
 from pets import pets_bp
 from adopt import adopt_bp
 from donate import donation_bp
+from schedule import schedule_bp
 
 app = Flask(__name__, static_url_path='/public', static_folder='public')
 swagger = Swagger(app)
@@ -50,6 +51,7 @@ app.register_blueprint(user_bp)
 app.register_blueprint(pets_bp)
 app.register_blueprint(adopt_bp)
 app.register_blueprint(donation_bp)
+app.register_blueprint(schedule_bp)
 
 @app.errorhandler(413)
 def file_too_large(_error):
