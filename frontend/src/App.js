084ba74efc128851e74322abd02e4162987009ab
Supeng Yu <supeng.yu@uconn.edu>
2024-11-20 20:26:16 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import ChangeProfile from './pages/ChangeProfile';
 import MyApplications from './pages/MyApplications';
 import ApplicationDetail from './pages/ApplicationDetail';
 import PetProfile from './pages/PetProfile';
+import StartApplication from "./pages/StartApplication";
 
 // Import other pages as needed
 
@@ -34,6 +35,7 @@ function App() {
         <Route path="/my-applications" element={<MyApplications/>} />
         <Route path="/application/:applicationId" element={<ApplicationDetail />} />
         <Route path="/PetProfile/:petId" element={<PetProfile />} />
+        <Route path="/start-application/:petId" element={<StartApplication />} />
         {/* Add other routes as needed */}
       </Routes>
     </Router>
